0f59b53efb846177a4254c576fab194f2fc5ccf4
JenkinsFan69 <williambglucky@gmail.com>
2023-05-01 17:06:28 +0700

    commitkedua

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
 }
 
 $conn->select_db($dbname);
-
+$count = 0;
 
 
 $sql = "SELECT ID, Nama, Alamat, Jabatan FROM users";
@@ -19,11 +19,13 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
+        $count = $count + 1;
         echo "ID: ". $row["ID"]. " - Nama: ". $row["Nama"]. " - Alamat " . $row["Alamat"]. " - Jabatan " . $row["Jabatan"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 
+echo "usercount: ".$count;
 $conn->close();
 ?>

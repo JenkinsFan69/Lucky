b861b61dc1d9deac2e1ed817d46072cb6d33d3fb
JenkinsFan69 <williambglucky@gmail.com>
2023-05-01 17:08:25 +0700

    commitkedua

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "password";
 $dbname = "redlock";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
